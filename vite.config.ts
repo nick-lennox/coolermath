1a96d2f62adeae3602dfc63f0a8e4753ddf23163
nick-lennox <44211696+nick-lennox@users.noreply.github.com>
2025-01-08 19:01:24 -0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  define: {
+    // Required for Excalidraw
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }
+  }
+});
